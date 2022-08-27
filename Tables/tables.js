9b4f4586d3b17bf9b083f5c727bbd60dd17fff41
priyanshu1607 <priyanshumathur215@gmail.com>
2022-08-27 11:59:58 +0530

    navigating in leaderboard added

---
+++ Tables/tables.js
@@ -0,0 +1,32 @@
+function week1(){
+    document.getElementById("navweek1").classList.add("active")
+
+    document.getElementById("navweek2").classList.remove("active")
+    document.getElementById("navweek3").classList.remove("active")
+    document.getElementById("navweek4").classList.remove("active")
+}
+
+function week2(){
+    document.getElementById("navweek1").classList.remove("active")
+
+    document.getElementById("navweek2").classList.add("active")
+    document.getElementById("navweek3").classList.remove("active")
+    document.getElementById("navweek4").classList.remove("active")
+}
+
+function week3(){
+    document.getElementById("navweek1").classList.remove("active")
+    document.getElementById("navweek2").classList.remove("active")
+
+    document.getElementById("navweek3").classList.add("active")
+
+    document.getElementById("navweek4").classList.remove("active")
+}
+
+function week4(){
+    document.getElementById("navweek1").classList.remove("active")
+    document.getElementById("navweek2").classList.remove("active")
+    document.getElementById("navweek3").classList.remove("active")
+    
+    document.getElementById("navweek4").classList.add("active")
+}
